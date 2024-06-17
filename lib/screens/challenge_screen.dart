e3ee053dcddf08ad4b21f02c4fe92191e7ca8b47
n-uwayo <n.uwayo@alustudent.com>
2024-06-17 12:22:47 +0200

    images

--- lib/screens/challenge_screen.dart
+++ lib/screens/challenge_screen.dart
@@ -16,7 +16,7 @@ class ChallengeScreen extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Image.network('https://via.placeholder.com/150'),
+              Image.network('https://s3-alpha-sig.figma.com/img/5f75/0b79/de310b80aa1c5dd6502eb8b4dcf69188?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=B7yx3zbMKH~uwddYOLtxIwdEwsGJQcTd7jxgIgJmSEKF9gAF5oeQEFADJhYDkY4IZPafJ70DR2SM0vOXU4a~~RFlW44GpQQOA3L9FM0G1dP8GHtpMpH87p949GCj1E2yR4NwhndB8DZsISBPKyZQKQWeVvY5hzaRDAjzTyngN0cUh3P5bVY6aiLkuixw5aFJPnhLC5Nk3tV9i4tSR8qLoWHO5RM0c7PP~tKvLZiIyMWmcLIL9qJp3bwAnGhoL30p~uNuLMh70aB~R3YuChVXZgrlqIsuRF0wQeidCNlhni~vhW5Ai6VQ~~3MDPTP-KghhmiB13fdX29qrFxynBztVg__'),
               SizedBox(height: 10),
               Text('Challenge Details', style: TextStyle(color: Colors.white)),
               SizedBox(height: 10),
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -29,8 +29,8 @@ class _ProfileScreenState extends State<ProfileScreen> with SingleTickerProvider
       appBar: AppBar(
         title: Row(
           children: [
-            Image.asset(
-              'assets/ew.png', 
+            Image.network(
+              'https://s3-alpha-sig.figma.com/img/278e/a5b3/0c6e41d72a229dc53bab8b7ea95ab849?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=f5nLJT5jKFPSE54ODAXKB68n~~bYtpdKzwRC4IG1RYEnPjm8UeT5D~x6TJJusnMlFayWtDMmKro1LE9GUf7BEuFaf0H8oxjVyJcAlr8qogpekpnp7ek-BZY5tloE53Wuu8ZCX5IM0MY2Ca-GgBc5BtUYsN6uGXPe0aKvu9lxU20U-ygXl-fOOrfnl1Ga8s0ttful~9Sh09zGB5TWCctiUC~AARMw2J7KPbF1eTNXyPaJkK5wmstcN-3P~UtGljJOV9udt6Ast9dl4BKKKD~F6DVIi7U~ZsWqmBe7OED5ZNmIMmhKfC0fs7AwSum5Rib-VnJZj0TT~cMAqQB2O-94yQ__', 
               height: 30,
             ),
             SizedBox(width: 8),
@@ -85,7 +85,7 @@ class _ProfileScreenState extends State<ProfileScreen> with SingleTickerProvider
             children: [
               CircleAvatar(
                 backgroundImage: NetworkImage(
-                    'https://storage.needpix.com/rsynced_images/profile-2092113_1280.png'),
+                    'https://s3-alpha-sig.figma.com/img/ab20/f316/d8e38efe2739a55964cec2aae334d474?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VQKt1v2175fXzD2Of5xmq6zWJqFZrzc5Os4hKGbd89SV7BbQafQT18Qb-pfEg-rgh0ZnxebblQRSWCDWOoXUXQSlsoW923gQLk8FQe8tVAcN7ejOkiK3Ou9E~xcJM8o2w~S3N9MuTtRG~5sa9MTLYWIk~7qZxAAZwUIaQI6UM4ReJOvd2pMZhOM1Ix5pOxOdjRJmbPj57oKlBrumVj-Vj46dIvEqvEvj3GWPe9TogwJ9FbnImAzBE7VkezUympjY7NHovzgFMnf~b6XlG-IT~4r~~FlQ8jq-jxTV2biM-mIU3x7XJiRfFvGa~ta8~1I4LCus67-dnhVtTD8oDUnqaA__'),
                 radius: 40,
               ),
               SizedBox(height: 20),
